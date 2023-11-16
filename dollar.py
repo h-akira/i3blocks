4403f9dd8def0c4896b73a589e44bc97d2366933
h-akira <79441679+h-akira@users.noreply.github.com>
2023-11-16 13:48:39 +0900

    change method to get rate

--- dollar.py
+++ dollar.py
@@ -4,81 +4,39 @@
 # Created: 2023-05-15 01:42:51
 
 # Import
-import os
-import json
 import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import requests
 import traceback
+import os
+from contextlib import redirect_stdout
 
 def parse_args():
   import argparse
   parser = argparse.ArgumentParser(description="""\
-
+ticker: https://optrip.xyz/?p=4467
 """, formatter_class = argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--version", action="version", version='%(prog)s 0.0.1')
-  parser.add_argument("--encoding", metavar="encoding", default="utf-8", help="encoding")
-  parser.add_argument("-e", "--error", action="store_true", help="print error message")
-  parser.add_argument("file", metavar="json-file", help="json file")
+  parser.add_argument("-r", "--round", metavar="桁数", type=int, default=3, help="小数点以下の桁数")
+  parser.add_argument("-t", "--ticker", metavar="ticker", default="USDJPY=X", help="ticker")
+  parser.add_argument("-n", "--name", metavar="name", default="USD/JPY", help="name")
   options = parser.parse_args()
-  if not os.path.isfile(options.file): 
-    raise Exception("The file does not exist.") 
   return options
-
-# def get_rate():
-#   URL = "https://www.click-sec.com/corp/guide/fxneo/rate/"
-#   CHROMEDRIVER = "/usr/bin/chromedriver"
-#   options = Options()
-#   options.add_argument('--headless')
-#   driver = webdriver.Chrome(CHROMEDRIVER, options=options)
-#   driver.get(URL)
-#   dollar_yen_buy = driver.find_element_by_id("fxneorate_bid_0").text
-#   driver.quit()
-#   return dollar_yen_buy
-
-def get_rate_AlphaVantage(KEY):
-  url = 'https://www.alphavantage.co/query'
-  params = {
-    'function': 'CURRENCY_EXCHANGE_RATE',
-    'from_currency': 'USD',
-    'to_currency': 'JPY',
-    'apikey': KEY
-  }
-  response = requests.get(url, params=params)
-  data = response.json()
-  return float(data["Realtime Currency Exchange Rate"]["8. Bid Price"])
-
-def market_open():
-  DIFF_JST_FROM_UTC = 9
-  now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-  num = now.weekday()
-  if 1 <= num <= 4:
-    return True
-  elif num == 0 and now.hour >= 7:  
-    return True
-  elif num == 5 and now.hour < 7:
-    return True
-  else:
-    return False
 
 def main():
   options = parse_args()
-  info = json.load(open(options.file, mode="r", encoding=options.encoding))
-  if market_open():
-    try:
-      # dollar_yen_buy = get_rate()
-      dollar_yen_buy = str(get_rate_AlphaVantage(info["dollar"]["KEY"]))
-      if len(dollar_yen_buy)>7:
-        dollar_yen_buy = dollar_yen_buy[:7]
-    except Exception:
-      if options.error:
-        print("=== Error ===")
-        traceback.print_exc()
-      dollar_yen_buy = "NULL"
-    print(f"USD/JPY={dollar_yen_buy}")
-  else:
-    print("Market is not open")
+  try:
+    import pandas_datareader.data as web
+    import yfinance as yf
+    yf.pdr_override()
+    start = datetime.date.today() - datetime.timedelta(days=7)
+    with redirect_stdout(open(os.devnull, 'w')):
+      df = web.get_data_yahoo(tickers=options.ticker,start=start)
+    price = df.iloc[-1]["Close"]
+  except Exception:
+    if options.error:
+      print("=== Error ===")
+      traceback.print_exc()
+    price = "NULL"
+  print(f"USD/JPY={round(price, options.round)}")
 
 if __name__ == '__main__':
   main()

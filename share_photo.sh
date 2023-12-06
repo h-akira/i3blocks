8bd57f605319bb239afd68ddeaddce46f87db393
h-akira <79441679+h-akira@users.noreply.github.com>
2023-12-07 02:03:41 +0900

    fix path

--- share_photo.sh
+++ share_photo.sh
@@ -3,7 +3,7 @@
 # Created:      2023-02-25 13:32:43
 set -eu
 
-STATE="`cat ${HOME}/.local/opt/Share_Photo/secret/state.txt`"
+STATE="`cat ${HOME}/.local/opt/SharePhoto/secret/state.txt`"
 if [ "${STATE}" != "Updated" ]; then
   COLOR_DOWN=${COLOR_DOWN:-#FF0000}
   echo "Photo: ${STATE}"
